df20cfe75bd080a9c2de2ae175b509f72c45d63d
Stephen Green <stephen.green.2021@mumail.ie>
2024-03-21 10:27:11 +0000

    First push

---
+++ game-one/src/components/Button.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { ReactComponent as ButtonSvg } from '../assets/Button.svg';
+
+const Button = () => {
+  return (
+    <div>
+      <ButtonSvg className="Button" />
+    </div>
+  );
+}
+
+export default Button;
