7491e634dc6d2e894ad320b42fe804d2aa632d6e
KEVIN MAURICE <kevin.maurice.2022@mumail.ie>
2024-06-26 23:06:45 +0100

    Initial commit

--- Project/src/Components/LightsOn/InGame.js
+++ Project/src/Components/LightsOn/InGame.js
@@ -1,11 +1,10 @@
-/* eslint-disable */
 
-//? REACT IMPORTS
+// REACT IMPORTS
 import { useLocation } from "react-router-dom";
 import { useState, useRef, useEffect } from "react";
 import Ball from "../ball/Ball";
 
-// ? STYLE IMPORTS
+//I IMPORTED ALL THE FILES / STYLE IMPORTS
 import BlackBoard from "../../Assets/board-layer-black-large.svg";
 import SmallBlackBoard from "../../Assets/board-layer-black-small.svg";
 import WhiteBoard from "../../Assets/board-layer-white-large.svg";
@@ -23,8 +22,8 @@ const InGame = () => {
   let winningArray = [
     // Array contents
     // Array of winning combinations
-    [0, 1, 2, 3],
-    [41, 40, 39, 38],
+    [0, 1, 2, 3], //would be top left to top right running horizontally
+    [41, 40, 39, 38], //etc
     [7, 8, 9, 10],
     [34, 33, 32, 31],
     [14, 15, 16, 17],
@@ -35,7 +34,7 @@ const InGame = () => {
     [13, 12, 11, 10],
     [35, 36, 37, 38],
     [6, 5, 4, 3],
-    [0, 7, 14, 21],
+    [0, 7, 14, 21], //represents a vertical win on the first column
     [41, 34, 27, 20],
     [1, 8, 15, 22],
     [40, 33, 26, 19],
@@ -49,7 +48,7 @@ const InGame = () => {
     [36, 29, 22, 15],
     [6, 13, 20, 27],
     [35, 28, 21, 14],
-    [0, 8, 16, 24],
+    [0, 8, 16, 24], //represents a diagonal win from the top left to the bottom right.
     [41, 33, 25, 17],
     [7, 15, 23, 31],
     [34, 26, 18, 10],
@@ -94,6 +93,7 @@ const InGame = () => {
     [13, 20, 27, 34],
   ];
   // Refs to various elements
+  //const button1 = useRef(); to const button7 = useRef();: These references correspond to the buttons used to drop the game pieces into the columns of the Connect 4 board.
   const boardRef = useRef();
   const button1 = useRef();
   const button2 = useRef();
@@ -103,22 +103,22 @@ const InGame = () => {
   const button6 = useRef();
   const button7 = useRef();
   // State variable
-  const [redPlayerBalls, setRedPlayerBalls] = useState([]);
-  const [yellowPlayerBalls, setYellowPlayerBalls] = useState([]);
-  const [slot, setSlot] = useState({ 0: 5, 1: 5, 2: 5, 3: 5, 4: 5, 5: 5, 6: 5 });
-  const [modalShow, setModalShow] = useState(false);
-  const [markerMove, setMarkerMove] = useState({ x: 200, y: 200 });
-  const [turn, setTurn] = useState(true);
-  const [balls, setBalls] = useState([]);
-  const [timer, setTimer] = useState(0);
-  const [winner, setWinner] = useState("");
-  const [winningBalls, setWinningBalls] = useState([]);
-  const [redScore, setRedScore] = useState(0);
-  const [yellowScore, setYellowScore] = useState(0);
-  const [cpuTurn, setcpuTurn] = useState(true);
-  const [isScreenSmall, setIsScreenSmall] = useState(false);
-  const [isGameFinished, setIsGameFinished] = useState(false);
-  const [isButtonDisabled, setIsButtonDisabled] = useState(false);
+  const [redPlayerBalls, setRedPlayerBalls] = useState([]); //This state keeps track of all the positions on the board where the red player has placed their pieces.
+  const [yellowPlayerBalls, setYellowPlayerBalls] = useState([]); //This state keeps track of all the positions on the board where the yellow player has placed their pieces.
+  const [slot, setSlot] = useState({ 0: 5, 1: 5, 2: 5, 3: 5, 4: 5, 5: 5, 6: 5 }); //This state keeps track of the next available row in each column. Each key represents a column, and its value represents the next available row in that column.
+  const [modalShow, setModalShow] = useState(false); //This state manages the visibility of a modal, which might be used for displaying messages or settings.
+  const [markerMove, setMarkerMove] = useState({ x: 200, y: 200 }); //This state tracks the position of a marker, probably for highlighting the current column where a piece will be dropped.
+  const [turn, setTurn] = useState(true); //This state manages whose turn it is to play. A value of true might represent the yellow player's turn, while false represents the red player's turn.
+  const [balls, setBalls] = useState([]); //This state keeps track of all the pieces on the board, including their position and color.
+  const [timer, setTimer] = useState(0); //This state is a timer that could be used to track the duration of the game or the time taken for each turn.
+  const [winner, setWinner] = useState(""); //This state keeps track of the winner of the game. It starts as an empty string and is set to the winning player's color when someone wins.
+  const [winningBalls, setWinningBalls] = useState([]); //This state stores the positions of the winning pieces when there is a winner.
+  const [redScore, setRedScore] = useState(0); //This state tracks the red player's score.
+  const [yellowScore, setYellowScore] = useState(0); //This state tracks the yellow player's score.
+  const [cpuTurn, setcpuTurn] = useState(true); //This state is used to manage the CPU's turn in a single-player game.
+  const [isScreenSmall, setIsScreenSmall] = useState(false); //This state determines whether the screen size is small, which might affect the layout or behavior of the game.
+  const [isGameFinished, setIsGameFinished] = useState(false); //This state indicates whether the game has finished.
+  const [isButtonDisabled, setIsButtonDisabled] = useState(false); //This state is used to enable or disable the column buttons, preventing multiple rapid clicks.
 
    // Get the location object to determine if it's a player or CPU
   const location = useLocation();
@@ -129,19 +129,19 @@ const InGame = () => {
       window.location.reload();
     }
     window.addEventListener("resize", handleResize);
-  }, [isScreenSmall]);
+  }, [isScreenSmall]); //for resizing the window and updating the state accordingly.
 
   useEffect(() => {
-    let newBalls = balls.map((ball) => {
+    let newBalls = balls.map((ball) => { // create a new array newBalls by mapping through the current balls array, which contains all the balls on the game board.
       if (
         String(ball.num) === String(winningBalls[0]) ||
-        String(ball.num) === String(winningBalls[1]) ||
+        String(ball.num) === String(winningBalls[1]) || //For each ball, we check if matches any of the numbers in the winningBalls array.
         String(ball.num) === String(winningBalls[2]) ||
         String(ball.num) === String(winningBalls[3])
       ) {
         return {
-          check: true,
-          color: ball.color,
+          check: true, //if true
+          color: ball.color, // visually highlight the winning balls on the game board.
           left: ball.left,
           num: ball.num,
           top: ball.top,
@@ -158,12 +158,12 @@ const InGame = () => {
     });
     setBalls(newBalls);
     if (winner === "red") {
-      setRedScore(redScore + 1);
+      setRedScore(redScore + 1); //updating scores
     } else {
       setYellowScore(yellowScore + 1);
     }
     setIsGameFinished(false);
-  }, [winningBalls, isGameFinished]);
+  }, [winningBalls, isGameFinished]); //reset function
 
   useEffect(() => {
     let macroTimer = setTimeout(() => {
@@ -174,7 +174,7 @@ const InGame = () => {
     };
   }, [timer]);
 
-  useEffect(() => {
+  useEffect(() => { //sets everything to 0
     setYellowPlayerBalls([]);
     setRedPlayerBalls([]);
     setRedScore(0);
@@ -189,7 +189,7 @@ const InGame = () => {
 
   const handlePlay = (event) => {
     setIsButtonDisabled(true);
-    setTimeout(() => setIsButtonDisabled(false), 600);
+    setTimeout(() => setIsButtonDisabled(false), 600); //disables button and re-anables afer 600 ms to stop rapid clicking
 
     if (slot[event.target.id] >= 0 && !winner) {
       if (!isScreenSmall) {
@@ -242,7 +242,7 @@ const InGame = () => {
     }
   }, [cpuTurn, winner]);
 
-  const handleReset = () => {
+  const handleReset = () => { //Resets the game state to start a new game 
     setBalls([]);
     setTimer(0);
     setWinner("");
@@ -251,38 +251,38 @@ const InGame = () => {
     setSlot({ 0: 5, 1: 5, 2: 5, 3: 5, 4: 5, 5: 5, 6: 5 });
   };
 
-  const handleRestart = () => {
+  const handleRestart = () => { //Resets the game state to start a new game and the score
     setBalls([]);
     setTimer(0);
     setWinner("");
     setRedPlayerBalls([]);
     setYellowPlayerBalls([]);
     setSlot({ 0: 5, 1: 5, 2: 5, 3: 5, 4: 5, 5: 5, 6: 5 });
-    setRedScore(0);
-    setYellowScore(0);
+    setRedScore(0); //<<
+    setYellowScore(0); //<<
   };
 
-  const updateDisplay = (event) => {
+  const updateDisplay = (event) => { //Updates the position of a marker based on mouse movement.
     setMarkerMove({ x: event.clientX, y: event.clientY });
   };
 
   useEffect(() => {
     let redCounter = 0;
-    let yellowCounter = 0;
-    for (let i = 0; i < winningArray.length; i++) {
-      for (let j = 0; j < winningArray[i].length; j++) {
+    let yellowCounter = 0; //These counters keep track of how many consecutive pieces each player has in the current winning combination being checked.
+    for (let i = 0; i < winningArray.length; i++) { // iterates through each combination in winningArray.
+      for (let j = 0; j < winningArray[i].length; j++) { // iterates through each position in the current winning combination.
         if (redPlayerBalls.includes(winningArray[i][j])) {
           redCounter++;
         } else if (yellowPlayerBalls.includes(winningArray[i][j])) {
           yellowCounter++;
         }
-        if (redCounter > 3) {
+        if (redCounter > 3) { //If redCounter exceeds 3, it means the red player has a winning combination. 
           setWinner("yellow");
           let won = winningArray[i];
           setWinningBalls(won);
           break;
         }
-        if (yellowCounter > 3) {
+        if (yellowCounter > 3) { //If yellowCounter exceeds 3, it means the yellow player has a winning combination. 
           setWinner("red");
           let won = winningArray[i];
           setWinningBalls(won);
@@ -294,18 +294,19 @@ const InGame = () => {
     }
   }, [turn]);
 
-  const cpuPlay = () => {
+  const cpuPlay = () => { //handles the logic for the CPU's move in the game.
     let emptySlot;
     let cpuNum;
-    while (true) {
-      let myNum = Math.floor(Math.random() * 6);
+    while (true) { //A while loop is used to randomly select a column (myNum) and check if there is an available slot (cpuSlot) in that column.
+      let myNum = Math.floor(Math.random() * 6); //Randomly selects a column index between 0 and 5.
       let cpuSlot = slot[myNum];
-      if (cpuSlot >= 0) {
+      if (cpuSlot >= 0) { // Checks if the selected column has an available slot.
         emptySlot = cpuSlot;
         cpuNum = myNum;
         break;
       }
     }
+    //changes coordinations according to the size of the screen
     if (!isScreenSmall) {
       setBalls([
         ...balls,
